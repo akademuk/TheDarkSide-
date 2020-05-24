4f903a8c8a56fac3bef31fef50a534f6d3d4bc98
Oleh Fedorov <akademuk24@gmail.com>
2020-05-24 11:58:25 +0300

    adaptive-img

--- scripts/fix.js
+++
@@ -1,6 +0,0 @@
-document.querySelector("button").addEventListener("click", (event) => {
-  document.body.classList.add("open");
-});
-document.querySelector(".overlay").addEventListener("click", (event) => {
-  document.body.classList.remove("open");
-});
---
+++ scripts/notscroll.js
@@ -0,0 +1,7 @@
+function showDialog() {
+  document.getElementsByTagName("body")[0].style.overflowY = "hidden";
+}
+
+function closeDialog() {
+  document.getElementsByTagName("body")[0].style.overflowY = "scroll";
+}
